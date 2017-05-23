b37395d5390d6838c99b935f703f2433b14f61aa
Brian M Williams <bmwilli1@comcast.net>
2017-05-23 14:44:43 +0000

    Add zk notes

---
+++ zk/runit.sh
@@ -0,0 +1,3 @@
+/opt/mesosphere/zookeeper/bin/zkCli.sh <<EOF
+`/opt/ibm/InfoSphere_Streams/4.2.0.0/java/bin/java -cp /opt/ibm/InfoSphere_Streams/4.2.0.0/system/impl/lib/com.ibm.streams.platform.jar com.ibm.streams.management.persistence.Permission StreamsDomain`
+EOF
